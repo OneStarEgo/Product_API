4aa9f795ed569c38e3acfcf4950e884587df298d
Rodney McGee <rodneymcgee73@gmail.com>
2022-07-22 12:45:20 -0500

    Added Image to Models and Serializers in Products App

--- products/models.py
+++ products/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.CharField(max_length=255)
